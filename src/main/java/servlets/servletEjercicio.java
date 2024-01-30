317ce920c8f2c53e3e4c734635d171afc994048d
Dani <correojl@gmail.com>
2024-01-30 20:49:38 +0100

    actu

---
+++ src/main/java/servlets/servletEjercicio.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class servletEjercicio {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String nombre = request.getParameter("Nombre");
+		String apellidos = request.getParameter("Apellidos");
+		String sexo = request.getParameter("Sexo");
+		String asignatura = request.getParameter("Asignatura");
+		String textArea = request.getParameter("textArea");	
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+			String nombre = request.getParameter("Nombre");
+			String apellidos = request.getParameter("Apellidos");
+			String sexo = request.getParameter("Sexo");
+			String [] asignatura = request.getParameterValues("Asignatura");
+			String desc = request.getParameter("textArea");
+			
+			Alumno alumno = new (nombre,apellidos,sexo,asignatura,desc);
+			
+			for (String asignatura : asignaturas) {
+				System.out.println(asignaturas);
+			}
+			
+			
+			response.getWriter().append("<h5> INSERTADO CORRECTAMENTE"+ nombre + apellidos + sexo + asignatura + desc + "</h5>");
+			response.getWriter().append("Nombre, Apellidos, Sexo, Asignatura, textArea");
+			
+	}
+	
+	
+}
